a031c2d088c685672e279d36bc3e498e97334486
Simon Tucker <simon@brotherlogic@gmail>
2019-07-13 12:30:57 -0700

    Adds files needed to record state

---
+++ frametrackerapi.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/frametracker/proto"
+)
+
+func (s *Server) RecordStatus(ctx context.Context, in *pb.StatusRequest) (*pb.StatusResponse, error) {
+	defer s.save(ctx)
+
+	for i, status := range s.config.States {
+		if status.TokenHash == in.Status.TokenHash {
+			s.config.States[i] = in.Status
+			return &pb.StatusResponse{}, nil
+		}
+	}
+
+	s.config.States = append(s.config.States, in.Status)
+	return &pb.StatusResponse{}, nil
+}
