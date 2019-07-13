ca08b37e970cd5d79542644e87c530d6ac5e5c5c
Simon Tucker <simon@brotherlogic@gmail>
2019-07-13 12:29:46 -0700

    Adds ability to record state

--- proto/frametracker.pb.go
+++ proto/frametracker.pb.go
@@ -6,6 +6,8 @@ package frametracker
 import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
 	math "math"
 )
 
@@ -61,6 +63,7 @@ func (m *Config) GetStates() []*Status {
 
 type Status struct {
 	TimeLastSync         int64    `protobuf:"varint,1,opt,name=time_last_sync,json=timeLastSync,proto3" json:"time_last_sync,omitempty"`
+	TokenHash            string   `protobuf:"bytes,2,opt,name=token_hash,json=tokenHash,proto3" json:"token_hash,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -98,22 +101,179 @@ func (m *Status) GetTimeLastSync() int64 {
 	return 0
 }
 
+func (m *Status) GetTokenHash() string {
+	if m != nil {
+		return m.TokenHash
+	}
+	return ""
+}
+
+type StatusRequest struct {
+	Status               *Status  `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *StatusRequest) Reset()         { *m = StatusRequest{} }
+func (m *StatusRequest) String() string { return proto.CompactTextString(m) }
+func (*StatusRequest) ProtoMessage()    {}
+func (*StatusRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_e6ec844a93fa6f21, []int{2}
+}
+
+func (m *StatusRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StatusRequest.Unmarshal(m, b)
+}
+func (m *StatusRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StatusRequest.Marshal(b, m, deterministic)
+}
+func (m *StatusRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StatusRequest.Merge(m, src)
+}
+func (m *StatusRequest) XXX_Size() int {
+	return xxx_messageInfo_StatusRequest.Size(m)
+}
+func (m *StatusRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_StatusRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StatusRequest proto.InternalMessageInfo
+
+func (m *StatusRequest) GetStatus() *Status {
+	if m != nil {
+		return m.Status
+	}
+	return nil
+}
+
+type StatusResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *StatusResponse) Reset()         { *m = StatusResponse{} }
+func (m *StatusResponse) String() string { return proto.CompactTextString(m) }
+func (*StatusResponse) ProtoMessage()    {}
+func (*StatusResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_e6ec844a93fa6f21, []int{3}
+}
+
+func (m *StatusResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_StatusResponse.Unmarshal(m, b)
+}
+func (m *StatusResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_StatusResponse.Marshal(b, m, deterministic)
+}
+func (m *StatusResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_StatusResponse.Merge(m, src)
+}
+func (m *StatusResponse) XXX_Size() int {
+	return xxx_messageInfo_StatusResponse.Size(m)
+}
+func (m *StatusResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_StatusResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_StatusResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*Config)(nil), "frametracker.Config")
 	proto.RegisterType((*Status)(nil), "frametracker.Status")
+	proto.RegisterType((*StatusRequest)(nil), "frametracker.StatusRequest")
+	proto.RegisterType((*StatusResponse)(nil), "frametracker.StatusResponse")
 }
 
 func init() { proto.RegisterFile("frametracker.proto", fileDescriptor_e6ec844a93fa6f21) }
 
 var fileDescriptor_e6ec844a93fa6f21 = []byte{
-	// 135 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4a, 0x2b, 0x4a, 0xcc,
-	0x4d, 0x2d, 0x29, 0x4a, 0x4c, 0xce, 0x4e, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2,
-	0x41, 0x16, 0x53, 0x32, 0xe3, 0x62, 0x73, 0xce, 0xcf, 0x4b, 0xcb, 0x4c, 0x17, 0xd2, 0xe1, 0x62,
-	0x2b, 0x2e, 0x49, 0x2c, 0x49, 0x2d, 0x96, 0x60, 0x54, 0x60, 0xd6, 0xe0, 0x36, 0x12, 0xd1, 0x43,
-	0xd1, 0x1c, 0x5c, 0x92, 0x58, 0x52, 0x5a, 0x1c, 0x04, 0x55, 0xa3, 0xa4, 0xc7, 0xc5, 0x06, 0x11,
-	0x11, 0x52, 0xe1, 0xe2, 0x2b, 0xc9, 0xcc, 0x4d, 0x8d, 0xcf, 0x49, 0x2c, 0x2e, 0x89, 0x2f, 0xae,
-	0xcc, 0x4b, 0x96, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x0e, 0xe2, 0x01, 0x89, 0xfa, 0x24, 0x16, 0x97,
-	0x04, 0x57, 0xe6, 0x25, 0x27, 0xb1, 0x81, 0x2d, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x68,
-	0x76, 0x9c, 0xa5, 0x92, 0x00, 0x00, 0x00,
+	// 236 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x90, 0x4d, 0x4b, 0xc3, 0x40,
+	0x10, 0x40, 0x8d, 0x85, 0x40, 0xc7, 0x58, 0x64, 0xf5, 0x10, 0xfc, 0x80, 0xb0, 0x78, 0xc8, 0x41,
+	0x7a, 0xa8, 0xe0, 0xcd, 0x93, 0x20, 0x82, 0x7a, 0x49, 0xbc, 0x87, 0xe9, 0x3a, 0x35, 0xa1, 0x76,
+	0xb7, 0xee, 0xcc, 0x0a, 0xfd, 0xf7, 0xd2, 0x6c, 0x0a, 0x0a, 0xc5, 0xeb, 0xdb, 0x7d, 0x33, 0x8f,
+	0x01, 0xb5, 0xf0, 0xb8, 0x22, 0xf1, 0x68, 0x96, 0xe4, 0xa7, 0x6b, 0xef, 0xc4, 0xa9, 0xec, 0x37,
+	0xd3, 0x77, 0x90, 0x3e, 0x38, 0xbb, 0xe8, 0x3e, 0xd4, 0x0d, 0xa4, 0x2c, 0x28, 0xc4, 0x79, 0x52,
+	0x8c, 0xca, 0xa3, 0xd9, 0xd9, 0xf4, 0x8f, 0x5c, 0x0b, 0x4a, 0xe0, 0x6a, 0xf8, 0xa3, 0x5f, 0x21,
+	0x8d, 0x44, 0x5d, 0xc3, 0x44, 0xba, 0x15, 0x35, 0x9f, 0xc8, 0xd2, 0xf0, 0xc6, 0x9a, 0x3c, 0x29,
+	0x92, 0x72, 0x54, 0x65, 0x5b, 0xfa, 0x82, 0x2c, 0xf5, 0xc6, 0x1a, 0x75, 0x05, 0x20, 0x6e, 0x49,
+	0xb6, 0x69, 0x91, 0xdb, 0xfc, 0xb0, 0x48, 0xca, 0x71, 0x35, 0xee, 0xc9, 0x13, 0x72, 0xab, 0xef,
+	0xe1, 0x78, 0x58, 0x40, 0x5f, 0x81, 0x58, 0x76, 0x35, 0x81, 0xfb, 0x69, 0xff, 0xd6, 0x04, 0xd6,
+	0x27, 0x30, 0xd9, 0xe9, 0xbc, 0x76, 0x96, 0x69, 0x36, 0x87, 0xd3, 0xc7, 0xad, 0xf0, 0x16, 0x85,
+	0x9a, 0xfc, 0x77, 0x67, 0x48, 0x3d, 0x43, 0x56, 0x91, 0x71, 0xfe, 0x7d, 0x88, 0xbf, 0xd8, 0x3b,
+	0x36, 0x36, 0x9c, 0x5f, 0xee, 0x7f, 0x8c, 0x1b, 0xf4, 0xc1, 0x3c, 0xed, 0x0f, 0x7a, 0xfb, 0x13,
+	0x00, 0x00, 0xff, 0xff, 0x89, 0xd1, 0x54, 0xdb, 0x66, 0x01, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// FrameTrackerServiceClient is the client API for FrameTrackerService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type FrameTrackerServiceClient interface {
+	RecordStatus(ctx context.Context, in *StatusRequest, opts ...grpc.CallOption) (*StatusResponse, error)
+}
+
+type frameTrackerServiceClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewFrameTrackerServiceClient(cc *grpc.ClientConn) FrameTrackerServiceClient {
+	return &frameTrackerServiceClient{cc}
+}
+
+func (c *frameTrackerServiceClient) RecordStatus(ctx context.Context, in *StatusRequest, opts ...grpc.CallOption) (*StatusResponse, error) {
+	out := new(StatusResponse)
+	err := c.cc.Invoke(ctx, "/frametracker.FrameTrackerService/RecordStatus", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// FrameTrackerServiceServer is the server API for FrameTrackerService service.
+type FrameTrackerServiceServer interface {
+	RecordStatus(context.Context, *StatusRequest) (*StatusResponse, error)
+}
+
+func RegisterFrameTrackerServiceServer(s *grpc.Server, srv FrameTrackerServiceServer) {
+	s.RegisterService(&_FrameTrackerService_serviceDesc, srv)
+}
+
+func _FrameTrackerService_RecordStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(StatusRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FrameTrackerServiceServer).RecordStatus(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/frametracker.FrameTrackerService/RecordStatus",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FrameTrackerServiceServer).RecordStatus(ctx, req.(*StatusRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _FrameTrackerService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "frametracker.FrameTrackerService",
+	HandlerType: (*FrameTrackerServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "RecordStatus",
+			Handler:    _FrameTrackerService_RecordStatus_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "frametracker.proto",
 }
