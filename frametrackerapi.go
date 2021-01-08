61d86ee8f70120a9fc40637573eb17c9a29292b7
Simon Tucker <brotherlogic@gmail.com>
2021-01-08 13:43:32 -0800

    Sets the time counter

--- frametrackerapi.go
+++ frametrackerapi.go
@@ -7,6 +7,15 @@ import (
 	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/frametracker/proto"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	frameAge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "frametracker_age",
+		Help: "ImageAge",
+	}, []string{"frame"})
 )
 
 //RecordStatus records a frame status
@@ -17,6 +26,8 @@ func (s *Server) RecordStatus(ctx context.Context, in *pb.StatusRequest) (*pb.St
 	}
 
 	s.Log(fmt.Sprintf("%v -> %v (%v)", in.Status.Origin, time.Now().Sub(time.Unix(in.Status.NewestFileDate/1000, 0)), in.Status.NewestFile))
+	frameAge.With(prometheus.Labels{"frame": in.Status.Origin}).Set(float64(time.Now().Sub(time.Unix(in.Status.NewestFileDate/1000, 0)).Seconds()))
+
 	if time.Now().Sub(time.Unix(in.Status.NewestFileDate/1000, 0)) > time.Hour*24*7 {
 		if in.Status.Origin != "natframe" {
 			s.RaiseIssue("Picture Frame Behind", fmt.Sprintf("%v is behind -> %v", in.Status.Origin, time.Unix(in.Status.NewestFileDate/1000, 0)))
