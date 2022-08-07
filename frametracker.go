03e0f79ff8f45a005facbac4bc3d350e15eb7552
Simon Tucker <brotherlogic@gmail.com>
2022-08-07 16:31:18 -0700

    Fix build error. This closes #153.

--- frametracker.go
+++ frametracker.go
@@ -83,9 +83,9 @@ func main() {
 		log.SetOutput(ioutil.Discard)
 	}
 	server := Init()
-	server.PrepServer()
+	server.PrepServer("frametracker")
 	server.Register = server
-	err := server.RegisterServerV2("frametracker", false, true)
+	err := server.RegisterServerV2(false)
 	if err != nil {
 		return
 	}
